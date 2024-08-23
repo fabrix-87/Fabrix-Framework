cd9d2c62005463847100c6e185ce1eb050538613
fabrix-87 <59539844+fabrix-87@users.noreply.github.com>
2024-08-23 19:17:03 +0200

    Security updates

--- Config/app.php
+++ Config/app.php
@@ -9,8 +9,8 @@ return [
     // Modalità debug
     'debug' => true,
     // Chiavi google captcha
-    'captcha_public_key' => '6LdaAskSAAAAAIXKD57dP_0Z3baKi1Jdd7_hviav',
-    'captcha_private_key' => '6LdaAskSAAAAADd-8Da9Be66HdDIPa8VDFX0lIe7',
+    'captcha_public_key' => '',
+    'captcha_private_key' => '',
     // Percorso del backend nelle route
     'backend_prefix' => 'admin',
     // Sottocartella del backend nelle cartelle dell'App (controller, model)
